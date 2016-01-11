c6db285d218a1f65aaa244831c75a836599d3cb8
Demo User <demouser@MacBook-Pro.local>
2016-01-11 11:47:12 -0500

    Added bootstrap and centered page.

--- superlists/functional_tests/tests.py
+++ superlists/functional_tests/tests.py
@@ -1,9 +1,10 @@
-from django.test import LiveServerTestCase
+#from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 #import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     # before every test, set up
     def setUp(self):
         self.browser = webdriver.Firefox()
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -26,3 +26,8 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list= list_)
     return redirect('/lists/%d/' % (list_.id,))
+
+def delete_item(request, item_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST['item_text'], list= list_)
+    return redirect('/lists/%d/' % (list_.id,))
