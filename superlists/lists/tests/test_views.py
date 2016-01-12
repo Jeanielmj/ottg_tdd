fb7613b76deee5789cd89c157a248bf534f75081
Demo User <demouser@MacBook-Pro.local>
2016-01-12 14:16:06 -0500

    Refractored list view to handle adding item on POST.

--- superlists/lists/tests/test_views.py
+++ superlists/lists/tests/test_views.py
@@ -48,13 +48,12 @@ class ListViewTest(TestCase):
         response = self.client.get('/lists/%d/' % (correct_list.id,))
         self.assertEqual(response.context['list'], correct_list)
 
-class NewItemTest(TestCase):
     def test_can_save_a_POST_request_to_an_existing_list(self):
         #other_list = List.objects.create()
         correct_list = List.objects.create()
 
         self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
+            '/lists/%d/' % (correct_list.id,),
             data={'item_text': 'A new item for an existing list'}
         )
 
@@ -62,16 +61,6 @@ class NewItemTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new item for an existing list')
         self.assertEqual(new_item.list, correct_list)
-
-    def test_redirects_to_list_view(self):
-        correct_list = List.objects.create()
-
-        response = self.client.post(
-            '/lists/%d/add_item' % (correct_list.id,),
-            data={'item_text': 'A new item for an existing list'}
-        )
-
-        self.assertRedirects(response, '/lists/%d/' % (correct_list.id,))
 
 class NewListTest(TestCase):
     def test_save_a_POST_request(self):
--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -23,14 +23,13 @@ def new_list(request):
     return redirect('/lists/%d/' % (new_list.id,))
 
 def view_list(request, list_id):
+
     list_ = List.objects.get(id=list_id)
     items = Item.objects.filter(list=list_)
+
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list= list_)
     return render(
         request, 'list.html',
         { 'items': items, 'list': list_ }
     )
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list= list_)
-    return redirect('/lists/%d/' % (list_.id,))
