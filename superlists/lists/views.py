ba5e5e6657c4e7dac08333fced01855bac790a65
Demo User <demouser@MacBook-Pro.local>
2016-01-12 14:38:21 -0500

    Apply validations when adding to a list.

--- superlists/lists/views.py
+++ superlists/lists/views.py
@@ -23,13 +23,18 @@ def new_list(request):
     return redirect('/lists/%d/' % (new_list.id,))
 
 def view_list(request, list_id):
-
     list_ = List.objects.get(id=list_id)
-    items = Item.objects.filter(list=list_)
+    error = None
 
     if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'], list= list_)
+        try:
+            item = Item(text=request.POST['item_text'], list= list_)
+            item.full_clean()
+            item.save()
+        except ValidationError:
+            error = "You can't have an empty list item"
+
     return render(
         request, 'list.html',
-        { 'items': items, 'list': list_ }
-    )
+        { 'list': list_, 'error': error }
+        )
